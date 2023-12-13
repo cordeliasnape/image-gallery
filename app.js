4e22ba163d6005e7d04c85c84bc76e0774872c76
cordelia <snapecordelia@gmail.com>
2023-12-13 15:58:14 +0000

    added dumb thing that doesnt work and took me so long to do

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-galleryImages = ["florence.jpg", "japan.jpg", "liverpool.jpg", "newyork.jpg", "riga.jpg", "singapore.jpg"]
+const galleryImage = document.getElementById("galleryImage");
 
+galleryObj = [
+  {
+    locName: "Florence",
+    url: "florence.jpg",
+    id: 1,
+  },
+  {
+    locName: "Japan",
+    url: "japan.jpg",
+    id: 2,
+  },
+  {
+    locName: "Liverpool",
+    url: "liverpool.jpg",
+    id: 3,
+  },
+  {
+    locName: "New York",
+    url: "newyork.jpg",
+    id: 4,
+  },
+  {
+    locName: "Riga",
+    url: "riga.jpg",
+    id: 5,
+  },
+  { locName: "Singapore", url: "singapore.jpg", id: 6 },
+];
 
+function displayImage() {
+  for (let i = 0; i < galleryObj.length; i++) {
+    console.log(galleryObj[i].url);
+
+    let img = document.createElement("img");
+    galleryImage.src = `"/assets/gallery/${galleryObj[i].url}"`;
+    img.appendChild(img);
+  }
+}
+displayImage();
 
 // TODO: loop through the images
 // TODO: DOM them onto the page
 // TODO: create thumbnail images
-// TODO: create collapsable media 
+// TODO: create collapsable media
