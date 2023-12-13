2b7e10b749a67ba2c8aeba87b2b7d93d4e366707
cordelia <snapecordelia@gmail.com>
2023-12-13 11:44:27 +0000

    added todo

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 galleryImages = ["florence.jpg", "japan.jpg", "liverpool.jpg", "newyork.jpg", "riga.jpg", "singapore.jpg"]
 
+
+
+// TODO: loop through the images
+// TODO: DOM them onto the page
+// TODO: create thumbnail images
+// TODO: create collapsable media 
